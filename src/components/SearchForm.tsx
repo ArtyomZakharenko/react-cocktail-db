26777ee68e10722331acbc87cd45971b8323165a
ArtyomZakharenko <artiomzaharenko@gmail.com>
2022-09-21 20:54:19 +0300

    implemented search form

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -4,10 +4,20 @@ import { useGlobalContext } from '../context'
 const SearchForm = () => {
   const { setSearchTerm } = useGlobalContext();
   return (
-    <div>
-      <h2>search form component</h2>
-    </div>
-  )
+    <section className="section search">
+      <form className="search-form">
+        <div className="form-control">
+          <label htmlFor="name">search your favorite cocktail</label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </div>
+      </form>
+    </section>
+  );
 }
 
 export default SearchForm

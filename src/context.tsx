b29cd5ec8c08fd31609dfe645e8d5bec45d69dee
ArtyomZakharenko <artiomzaharenko@gmail.com>
2022-09-21 20:20:58 +0300

    Context API setup

--- src/context.tsx
+++ src/context.tsx
@@ -1,15 +1,29 @@
-import { useState, useContext, useEffect, ReactNode, createContext } from 'react'
-
+import { createContext, ReactNode, useContext, useState } from 'react'
 
 const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
 const AppContext = createContext(null);
 
-const AppProvider = ({ children } : {children: ReactNode}) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
+const AppProvider = ({ children }: { children: ReactNode }) => {
+	const [loading, setLoading] = useState(false);
+	const [searchTerm, setSearchTerm] = useState('a');
+	const [cocktails, setCocktails] = useState([]);
+
+	return (
+		<AppContext.Provider
+			value={{
+				loading,
+				cocktails,
+				setLoading,
+				setCocktails
+			}}
+		>
+			{children}
+		</AppContext.Provider>
+	);
 }
-// make sure use
+
 export const useGlobalContext = () => {
-  return useContext(AppContext)
+	return useContext(AppContext);
 }
 
 export { AppContext, AppProvider }

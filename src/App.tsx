703ebd3b1f1ed63313694f39992ba46a3403a950
ArtyomZakharenko <artiomzaharenko@gmail.com>
2022-09-21 17:18:37 +0300

    added styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Error from "./pages/Error";
+import SingleCocktail from "./pages/SingleCocktail";
+import About from "./pages/About";
 
 function App() {
-  return (<div>Test</div>);
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='about' element={<About />} />
+        <Route path='cocktail/:id' element={<SingleCocktail />} />
+        <Route path='*' element={<Error />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;

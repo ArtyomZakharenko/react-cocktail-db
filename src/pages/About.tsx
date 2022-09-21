ca117756035e7ac3a22cf0e58f6cd02e365b1b41
ArtyomZakharenko <artiomzaharenko@gmail.com>
2022-09-21 17:27:49 +0300

    implemented error and about pages

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
-
 const About = () => {
-  return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
+	return (
+		<section className='section about-section'>
+			<h1 className='section-title'>about us</h1>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. A adipisci aliquid autem debitis dolore dolorem esse
+				eum, excepturi fugit nam necessitatibus nulla, odio perspiciatis placeat recusandae tempore tenetur veniam,
+				voluptate.
+			</p>
+		</section>
+	);
 }
 
 export default About
